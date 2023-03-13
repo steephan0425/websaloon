3216d0594553ffe429af7dc8805a361dc0a8659f
steephan0425 <127721968+steephan0425@users.noreply.github.com>
2023-03-13 12:39:56 +0530

    Add files via upload
    ok

---
+++ index.js
@@ -0,0 +1,8 @@
+
+
+const btnshow=document.querySelector('.btn-show');
+const navside=document.querySelector('.navside');
+
+btnshow.addEventListener('click',()=>{ 
+ navside.classList.toggle('show');
+ });
